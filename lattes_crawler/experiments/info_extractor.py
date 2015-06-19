f47d70292fd62552d9abcb26bc0225eee5ec6198
Jonathas Magalhães <jon_magal@yahoo.com.br>
2015-06-19 13:50:53 -0300

    Working Extractor with Some Bugs

--- lattes_crawler/experiments/info_extractor.py
+++ lattes_crawler/experiments/info_extractor.py
@@ -4,6 +4,7 @@
 '''
 
 import os
+from lattes_crawler.apps import research
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'lattes_crawler.settings'
 
@@ -21,6 +22,8 @@ def save_info(research, data_type, description, year):
     info.research      = research
     info.data_type     = data_type
     info.description   = description
+    if year == "Atual":
+        year = 2016
     info.year          = year
     info.save()
 
@@ -34,7 +37,7 @@ def extract_information(research):
     pesquisador         = curriculo_lattes.getElementsByTagName("pesquisador")[0]
     formacao_academica  = pesquisador.getElementsByTagName("formacao_academica")[0]
     formacao            = formacao_academica.getElementsByTagName("formacao")
-
+    print 'Extracting ID: '+research.lattes_id+' information...' 
     #   Doutorado, Mestrado e Graduação
     for form in formacao:
         ano_inicio          = form.getElementsByTagName("ano_inicio")
@@ -61,95 +64,107 @@ def extract_information(research):
         save_info(research = research, data_type = tipo_info, description = area, year = ano_conclusao_info)    
             
     #   Projetos
-    projetos_pesquisa = pesquisador.getElementsByTagName("projetos_pesquisa")[0]
-    projetos           = projetos_pesquisa.getElementsByTagName("projeto")
-
-    print "\nProjetos:\n"
-
-    for proj in projetos:
-        nome                      = proj.getElementsByTagName("nome")[0].firstChild.data
-        projeto_ano_inicio        = proj.getElementsByTagName("ano_inicio")[0].firstChild.data
-        projeto_ano_conclusao     = proj.getElementsByTagName("ano_conclusao")[0].firstChild.data
-
-        save_info(research = research, data_type = 'Projeto', description = nome, year = projeto_ano_conclusao)
-        
+    if pesquisador.getElementsByTagName("projetos_pesquisa"):
+        projetos_pesquisa = pesquisador.getElementsByTagName("projetos_pesquisa")[0]
+        projetos           = projetos_pesquisa.getElementsByTagName("projeto")
+    
+        print "\nProjetos:\n"
+    
+        for proj in projetos:
+            nome                      = proj.getElementsByTagName("nome")[0].firstChild.data
+            projeto_ano_inicio        = proj.getElementsByTagName("ano_inicio")[0].firstChild.data
+            projeto_ano_conclusao     = proj.getElementsByTagName("ano_conclusao")[0].firstChild.data
+    
+            save_info(research = research, data_type = 'Projeto', description = nome, year = projeto_ano_conclusao)
+            
     #   Livros
-    capitulos_livros              = pesquisador.getElementsByTagName("capitulos_livros")[0]
-    capitulo                      = capitulos_livros.getElementsByTagName("capitulo")
-    print"\nLivros:\n"
-
-    for cap in capitulo:
-        titulo_livro              = cap.getElementsByTagName("titulo")[0].firstChild.data
-        ano_livro                 = cap.getElementsByTagName("ano")[0].firstChild.data
-        
-        save_info(research = research, data_type = 'Livro', description = titulo_livro, year = ano_livro)
+    if pesquisador.getElementsByTagName("capitulos_livros"):
+        capitulos_livros              = pesquisador.getElementsByTagName("capitulos_livros")[0]
+        capitulo                      = capitulos_livros.getElementsByTagName("capitulo")
+        print"\nLivros:\n"
+    
+        for cap in capitulo:
+            titulo_livro              = cap.getElementsByTagName("titulo")[0].firstChild.data
+            ano_livro                 = cap.getElementsByTagName("ano")[0].firstChild.data
+            
+            save_info(research = research, data_type = 'Livro', description = titulo_livro, year = ano_livro)
         
     #   Texto em Jornals
-    texto_em_jornal = pesquisador.getElementsByTagName("texto_em_jornal")[0]
-    texto           = texto_em_jornal.getElementsByTagName("texto")
-    print"\nTexto em Jornals:\n"
-
-    for text in texto:
-        titulo_text               = text.getElementsByTagName("titulo")[0].firstChild.data
-        ano                       = text.getElementsByTagName("ano")[0].firstChild.data
-
-        save_info(research = research, data_type = 'Texto em Jornal', description = titulo_text, year = ano)
+    if pesquisador.getElementsByTagName("texto_em_jornal"):
+        texto_em_jornal = pesquisador.getElementsByTagName("texto_em_jornal")[0]
+        texto           = texto_em_jornal.getElementsByTagName("texto")
+        print"\nTexto em Jornals:\n"
+    
+        for text in texto:
+            titulo_text               = text.getElementsByTagName("titulo")[0].firstChild.data
+            ano                       = text.getElementsByTagName("ano")[0].firstChild.data
+    
+            save_info(research = research, data_type = 'Texto em Jornal', description = titulo_text, year = ano)
 
     #   Papers
-    trabalho_completo_congresso = pesquisador.getElementsByTagName("trabalho_completo_congresso")[0]
-    trabalho_completo           = trabalho_completo_congresso.getElementsByTagName("trabalho_completo")
-    print"\nPapers:\n"
-
-    for trabalho in trabalho_completo:
-        titulo_trabalho = trabalho.getElementsByTagName("titulo")[0].firstChild.data
-        nome_evento     = trabalho.getElementsByTagName("nome_evento")[0].firstChild.data
-        ano_trabalho    = trabalho.getElementsByTagName("ano")[0].firstChild.data
-
-        save_info(research = research, data_type = 'Paper', description = titulo_trabalho+' '+'('+nome_evento+')', year = ano_trabalho)
+    if pesquisador.getElementsByTagName("trabalho_completo_congresso"):
+        trabalho_completo_congresso = pesquisador.getElementsByTagName("trabalho_completo_congresso")[0]
+        trabalho_completo           = trabalho_completo_congresso.getElementsByTagName("trabalho_completo")
+        print"\nPapers:\n"
+    
+        for trabalho in trabalho_completo:
+            titulo_trabalho = trabalho.getElementsByTagName("titulo")[0].firstChild.data
+            nome_evento     = trabalho.getElementsByTagName("nome_evento")[0].firstChild.data
+            ano_trabalho    = trabalho.getElementsByTagName("ano")[0].firstChild.data
+    
+            save_info(research = research, data_type = 'Paper', description = titulo_trabalho, year = ano_trabalho)
 
     #   Resumo Expandido Congresso
-    resumo_expandido_congresso = pesquisador.getElementsByTagName("resumo_expandido_congresso")[0]
-    resumo_expandido           = resumo_expandido_congresso.getElementsByTagName("resumo_expandido")
-    print"\nResumos expandidos:\n"
-
-    for resumo in resumo_expandido:
-        titulo_resumo_expandido          = resumo.getElementsByTagName("titulo")[0].firstChild.data
-        nome_evento_resumo_expandido     = resumo.getElementsByTagName("nome_evento")[0].firstChild.data
-        ano_resumo_expandido             = resumo.getElementsByTagName("ano")[0].firstChild.data
-        
-        save_info(research = research, data_type = 'Resumo Expandido', description = titulo_resumo_expandido+' '+'('+nome_evento_resumo_expandido+')', year = ano_resumo_expandido)
+    if pesquisador.getElementsByTagName("resumo_expandido_congresso"):
+        resumo_expandido_congresso = pesquisador.getElementsByTagName("resumo_expandido_congresso")[0]
+        resumo_expandido           = resumo_expandido_congresso.getElementsByTagName("resumo_expandido")
+        print"\nResumos expandidos:\n"
+    
+        for resumo in resumo_expandido:
+            titulo_resumo_expandido          = resumo.getElementsByTagName("titulo")[0].firstChild.data
+            nome_evento_resumo_expandido     = resumo.getElementsByTagName("nome_evento")[0].firstChild.data
+            ano_resumo_expandido             = resumo.getElementsByTagName("ano")[0].firstChild.data
+            
+            save_info(research = research, data_type = 'Resumo Expandido', description = titulo_resumo_expandido, year = ano_resumo_expandido)
                 
     #   Resumo Congresso
-    resumo_congresso = pesquisador.getElementsByTagName("resumo_congresso")[0]
-    resum            = resumo_congresso.getElementsByTagName("resumo")
-    print"\nResumos:\n"
-
-    for r in resum:
-        titulo_resumo          = r.getElementsByTagName("titulo")[0].firstChild.data
-        nome_evento_resumo     = r.getElementsByTagName("nome_evento")[0].firstChild.data
-        ano_resumo             = r.getElementsByTagName("ano")[0].firstChild.data
+    if pesquisador.getElementsByTagName("resumo_congresso"):
+        resumo_congresso = pesquisador.getElementsByTagName("resumo_congresso")[0]
+        resum            = resumo_congresso.getElementsByTagName("resumo")
+        print"\nResumos:\n"
         
-        save_info(research = research, data_type = 'Resumo Congresso', description = titulo_resumo+' '+'('+nome_evento_resumo+')', year = ano_resumo)
+        for r in resum:
+            titulo_resumo          = r.getElementsByTagName("titulo")[0].firstChild.data
+            nome_evento_resumo     = None
+            if r.getElementsByTagName("nome_evento")[0].firstChild is not None:
+                nome_evento_resumo = r.getElementsByTagName("nome_evento")[0].firstChild.data
+            ano_resumo             = r.getElementsByTagName("ano")[0].firstChild.data
+            
+            save_info(research = research, data_type = 'Resumo Congresso', description = titulo_resumo, year = ano_resumo)
 
     #   Apresentação de Trabalhos
-    apresentacao_trabalho         = pesquisador.getElementsByTagName("apresentacao_trabalho")[0]
-    trabalho_apresentado          = apresentacao_trabalho.getElementsByTagName("trabalho_apresentado")
-    print"\nTrabalhos Apresentados:\n"
-
-    for trab in trabalho_apresentado:
-        titulo_trabalho_apresentado     = trab.getElementsByTagName("titulo")[0].firstChild.data
-        ano_trabalho_apresentado        = trab.getElementsByTagName("ano")[0].firstChild.data
-
-        save_info(research = research, data_type = 'Trabalho Apresentado', description = titulo_trabalho_apresentado, year = ano_trabalho_apresentado)
+    if  pesquisador.getElementsByTagName("apresentacao_trabalho"):
+        apresentacao_trabalho         = pesquisador.getElementsByTagName("apresentacao_trabalho")[0]
+        trabalho_apresentado          = apresentacao_trabalho.getElementsByTagName("trabalho_apresentado")
+        print"\nTrabalhos Apresentados:\n"
     
+        for trab in trabalho_apresentado:
+            titulo_trabalho_apresentado     = trab.getElementsByTagName("titulo")[0].firstChild.data
+            ano_trabalho_apresentado        = trab.getElementsByTagName("ano")[0].firstChild.data
+    
+            save_info(research = research, data_type = 'Trabalho Apresentado', description = titulo_trabalho_apresentado, year = ano_trabalho_apresentado)
+        
 
 def main():
-    research_list = Research.objects.exclude(lattes_information = None)
+    researches_with_info = ResearchInfo.objects.all().values("research_id").distinct()
+    
+    research_list        = Research.objects.exclude(lattes_information = None, id__in=researches_with_info).values('id')
     if not research_list:
         return
-    for research in research_list:
+    for research_id in research_list:
+        research = Research.objects.get(id = research_id['id'])
         extract_information(research)
-    
+        
 if __name__ == '__main__':
     main()
     
