6cfac7bdb93c274403c11a5c86783b5218315bab
Jonathas Magalhães <jon_magal@yahoo.com.br>
2015-06-23 11:04:02 -0300

    Working Extractor 1.0

--- lattes_crawler/experiments/info_extractor.py
+++ lattes_crawler/experiments/info_extractor.py
@@ -32,36 +32,40 @@ def extract_information(research):
     Extracts valuable information from a Researcher CV Lattes
     @param research: Research class object.
     """
+    print 'Extracting ID: '+research.lattes_id+' information...\n'
+    
     parsedinfo          = minidom.parseString(research.lattes_information.encode('utf8'))
     curriculo_lattes    = parsedinfo.getElementsByTagName("curriculo_lattes")[0]
     pesquisador         = curriculo_lattes.getElementsByTagName("pesquisador")[0]
-    formacao_academica  = pesquisador.getElementsByTagName("formacao_academica")[0]
-    formacao            = formacao_academica.getElementsByTagName("formacao")
-    print 'Extracting ID: '+research.lattes_id+' information...' 
+    
     #   Doutorado, Mestrado e Graduação
-    for form in formacao:
-        ano_inicio          = form.getElementsByTagName("ano_inicio")
-        ano_conclusao       = form.getElementsByTagName("ano_conclusao")
-        tipo                = form.getElementsByTagName("tipo")
-        
-        if tipo[0].firstChild is None:
-            pass
-        
-        else:
-            if ano_inicio[0].firstChild is None:
-                ano_inicio_info     = None
+    print "Formação:\n"
+    if pesquisador.getElementsByTagName("formacao_academica"):
+        formacao_academica  = pesquisador.getElementsByTagName("formacao_academica")[0]
+        formacao            = formacao_academica.getElementsByTagName("formacao")
+    
+        for form in formacao:
+            ano_inicio          = form.getElementsByTagName("ano_inicio")
+            ano_conclusao       = form.getElementsByTagName("ano_conclusao")
+            tipo                = form.getElementsByTagName("tipo")
+            
+            if tipo[0].firstChild is None:
+                pass
             else:
-                ano_inicio_info     = ano_inicio[0].firstChild.data
-        
-            if ano_conclusao[0].firstChild is None:
-                ano_conclusao_info  = None
-            else:
-                ano_conclusao_info  = ano_conclusao[0].firstChild.data
-
-            tipo_info            = tipo[0].firstChild.data.split(' ')[0]
-            area                 = ''.join(tipo[0].firstChild.data[1:])
-        
-        save_info(research = research, data_type = tipo_info, description = area, year = ano_conclusao_info)    
+                if ano_inicio[0].firstChild is None:
+                    ano_inicio_info     = None
+                else:
+                    ano_inicio_info     = ano_inicio[0].firstChild.data
+            
+                if ano_conclusao[0].firstChild is None:
+                    ano_conclusao_info  = None
+                else:
+                    ano_conclusao_info  = ano_conclusao[0].firstChild.data
+                tipo_info            = tipo[0].firstChild.data.split(' ')[0]
+                area                 = ''.join(tipo[0].firstChild.data[1:])
+            
+            save_info(research = research, data_type = tipo_info, description = area, year = ano_conclusao_info)
+        print "OK"    
             
     #   Projetos
     if pesquisador.getElementsByTagName("projetos_pesquisa"):
@@ -76,6 +80,7 @@ def extract_information(research):
             projeto_ano_conclusao     = proj.getElementsByTagName("ano_conclusao")[0].firstChild.data
     
             save_info(research = research, data_type = 'Projeto', description = nome, year = projeto_ano_conclusao)
+        print "OK"
             
     #   Livros
     if pesquisador.getElementsByTagName("capitulos_livros"):
@@ -84,11 +89,13 @@ def extract_information(research):
         print"\nLivros:\n"
     
         for cap in capitulo:
-            titulo_livro              = cap.getElementsByTagName("titulo")[0].firstChild.data
+            if cap.getElementsByTagName("titulo")[0].firstChild is not None:
+                titulo_livro              = cap.getElementsByTagName("titulo")[0].firstChild.data
             ano_livro                 = cap.getElementsByTagName("ano")[0].firstChild.data
             
             save_info(research = research, data_type = 'Livro', description = titulo_livro, year = ano_livro)
-        
+        print "OK"
+               
     #   Texto em Jornals
     if pesquisador.getElementsByTagName("texto_em_jornal"):
         texto_em_jornal = pesquisador.getElementsByTagName("texto_em_jornal")[0]
@@ -96,11 +103,13 @@ def extract_information(research):
         print"\nTexto em Jornals:\n"
     
         for text in texto:
-            titulo_text               = text.getElementsByTagName("titulo")[0].firstChild.data
+            if text.getElementsByTagName("titulo")[0].firstChild is not None:
+                titulo_text               = text.getElementsByTagName("titulo")[0].firstChild.data
             ano                       = text.getElementsByTagName("ano")[0].firstChild.data
     
             save_info(research = research, data_type = 'Texto em Jornal', description = titulo_text, year = ano)
-
+        print "OK"
+            
     #   Papers
     if pesquisador.getElementsByTagName("trabalho_completo_congresso"):
         trabalho_completo_congresso = pesquisador.getElementsByTagName("trabalho_completo_congresso")[0]
@@ -109,11 +118,11 @@ def extract_information(research):
     
         for trabalho in trabalho_completo:
             titulo_trabalho = trabalho.getElementsByTagName("titulo")[0].firstChild.data
-            nome_evento     = trabalho.getElementsByTagName("nome_evento")[0].firstChild.data
             ano_trabalho    = trabalho.getElementsByTagName("ano")[0].firstChild.data
     
             save_info(research = research, data_type = 'Paper', description = titulo_trabalho, year = ano_trabalho)
-
+        print "OK"
+            
     #   Resumo Expandido Congresso
     if pesquisador.getElementsByTagName("resumo_expandido_congresso"):
         resumo_expandido_congresso = pesquisador.getElementsByTagName("resumo_expandido_congresso")[0]
@@ -122,10 +131,11 @@ def extract_information(research):
     
         for resumo in resumo_expandido:
             titulo_resumo_expandido          = resumo.getElementsByTagName("titulo")[0].firstChild.data
-            nome_evento_resumo_expandido     = resumo.getElementsByTagName("nome_evento")[0].firstChild.data
+            #nome_evento_resumo_expandido     = resumo.getElementsByTagName("nome_evento")[0].firstChild.data
             ano_resumo_expandido             = resumo.getElementsByTagName("ano")[0].firstChild.data
             
             save_info(research = research, data_type = 'Resumo Expandido', description = titulo_resumo_expandido, year = ano_resumo_expandido)
+        print "OK"
                 
     #   Resumo Congresso
     if pesquisador.getElementsByTagName("resumo_congresso"):
@@ -141,7 +151,8 @@ def extract_information(research):
             ano_resumo             = r.getElementsByTagName("ano")[0].firstChild.data
             
             save_info(research = research, data_type = 'Resumo Congresso', description = titulo_resumo, year = ano_resumo)
-
+        print "OK"
+            
     #   Apresentação de Trabalhos
     if  pesquisador.getElementsByTagName("apresentacao_trabalho"):
         apresentacao_trabalho         = pesquisador.getElementsByTagName("apresentacao_trabalho")[0]
@@ -153,7 +164,8 @@ def extract_information(research):
             ano_trabalho_apresentado        = trab.getElementsByTagName("ano")[0].firstChild.data
     
             save_info(research = research, data_type = 'Trabalho Apresentado', description = titulo_trabalho_apresentado, year = ano_trabalho_apresentado)
-        
+        print "OK"
+    print"\n"
 
 def main():
     researches_with_info = ResearchInfo.objects.all().values("research_id").distinct()
@@ -167,5 +179,7 @@ def main():
         
 if __name__ == '__main__':
     main()
+    #research = Research.objects.get(lattes_id = "6160004433931781")
+    #extract_information(research)
     
     
